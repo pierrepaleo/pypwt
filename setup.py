0eb8c2f1004931ae78501cb7a0c15ba0f087cdf7
Pierre Paleo <pierre.paleo@esrf.fr>
2016-02-25 14:37:17 +0100

    switch to version 0.3

--- setup.py
+++ setup.py
@@ -149,7 +149,7 @@ class custom_build_ext(build_ext):
 setup(name='pypwt',
       # random metadata. there's more you can supply
       author='Pierre Paleo',
-      version='0.2',
+      version='0.3',
 
       ext_modules = [ext],
 
