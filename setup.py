74692bb3fc507412e55fc5409e9cabedab21c593
Pierre Paleo <pierre.paleo@esrf.fr>
2016-09-20 13:35:24 +0200

    modified name pdwt and setup.py

--- setup.py
+++ setup.py
@@ -88,7 +88,8 @@ except AttributeError:
     numpy_include = numpy.get_numpy_include()
 
 ext = Extension('pypwt',
-                sources=['ppdwt/wt.cu', 'ppdwt/common.cu', 'src/pypwt.pyx'],
+                #~ sources=['ppdwt/wt.cu', 'ppdwt/common.cu', 'src/pypwt.pyx'],
+                sources = ['pdwt/wt.cu', 'pdwt/common.cu', 'pdwt/utils.cu', 'pdwt/separable.cu', 'pdwt/nonseparable.cu', 'pdwt/haar.cu', 'pdwt/filters.cpp', 'src/pypwt.pyx']
                 #~ library_dirs=[CUDA['lib64']],
                 libraries=['cudart', 'cublas'],
                 language='c++',
